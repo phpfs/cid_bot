70300ea575a7f12d99c1c30fc5b03713fe3bd11b
phpfs <phpfs@users.noreply.github.com>
2018-06-30 18:15:08 +0200

    Preparation for Heroku

--- main.go
+++ main.go
@@ -5,27 +5,55 @@ import(
 	"log"
 	"strings"
 	"strconv"
+	"os"
+	"net/http"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("Token")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Panic("$PORT must be set!")
+	}
+
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		log.Panic("$TOKEN must be set!")
+	}
+
+	url := os.Getenv("URL")
+	if url == "" {
+		log.Panic("$URL must be set!")
+	}
+
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		log.Panic("$SECRET must be set!")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	log.Printf("Authorized on account %s!\n", bot.Self.UserName)
 
-	updates, err := bot.GetUpdatesChan(u)
-  	if err != nil {
-    		log.Panic(err)
-  	}
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(url + "/" + secret))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.LastErrorDate != 0 {
+		log.Printf("[Telegram callback failed]%s", info.LastErrorMessage)
+	}
+
+	updates := bot.ListenForWebhook("/" + secret)
+	go http.ListenAndServe("0.0.0.0:" + port, nil)
 
 	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-
 		if(strings.Contains(update.Message.Text, "/start")){
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome to @cid_bot!\n\nStart by querying your Telegram Chat_ID:\n/chatid\n\nIf you want to know something about this bot, send:\n/about\n\nGreetings, phpfs")
 			bot.Send(msg)
